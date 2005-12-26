e71e89cb5527799bfeb6c0f982aaae59f1be6933
Kohsuke Kawaguchi <kk@kohsuke.org>
2005-12-26 19:43:14 +0000

    javadoc improvement.

--- src/org/kohsuke/rngom/ast/builder/ElementAnnotationBuilder.java
+++ src/org/kohsuke/rngom/ast/builder/ElementAnnotationBuilder.java
@@ -4,7 +4,9 @@ import org.kohsuke.rngom.ast.om.Location;
 import org.kohsuke.rngom.ast.om.ParsedElementAnnotation;
 import org.kohsuke.rngom.ast.om.ParsedPattern;
 
-
+/**
+ * Used to build foreign element annotations.
+ */
 public interface ElementAnnotationBuilder<
     P extends ParsedPattern,
     E extends ParsedElementAnnotation,
@@ -12,6 +14,13 @@ public interface ElementAnnotationBuilder<
     A extends Annotations<E,L,CL>,
     CL extends CommentList<L>> extends Annotations<E,L,CL> {
 
+    /**
+     * Called when a child text is found.
+     */
     void addText(String value, L loc, CL comments) throws BuildException;
+
+    /**
+     * Called at the end to build an application data structure.
+     */
     E makeElementAnnotation() throws BuildException;
 }
